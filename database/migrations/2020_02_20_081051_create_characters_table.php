c7bf6b7ab92b197dc821e33495ab20b9fc821c88
otwald <hinterlanddev@googlemail.com>
2020-02-20 09:19:15 +0100

    added new table to hold basic character stats

---
+++ database/migrations/2020_02_20_081051_create_characters_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unique();
+            $table->string('name');
+            $table->string('home_village');
+            $table->string('current_location');
+            $table->string('faction');
+            $table->integer('age');
+            $table->string('picture')->nullable();
+            $table->string('rank')->default('Genin');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
