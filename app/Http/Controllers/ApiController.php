30044d3811ee61eee5f7cedf470cf3a44a58f62b
otwald <hinterlanddev@googlemail.com>
2020-02-20 08:59:15 +0100

    init

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class ApiController
+ * @package App\Http\Controllers
+ * 
+ * @SWG\Swagger(
+ *  basePath="",
+ *  host="localhost:8081",
+ *  schemes={"http"},
+ *  @SWG\Info(
+ *      version="1.0",
+ *      title="NarutoRPG",
+ *      @SWG\Contact (name="Developer Contact",
+ *          url="https://www.example.com")
+ *  )
+ * )
+ */
+class ApiController extends Controller
+{
+    //
+}
