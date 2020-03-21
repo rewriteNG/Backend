63e4c61a79acfdf10a6ebe90f0880809b1cafb6a
otwald <hinterlanddev@googlemail.com>
2020-03-21 09:54:00 +0100

    register now returns an accesstoken

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Moduls\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
@@ -73,25 +74,38 @@ class UserController extends Controller
      * Get the token array structure.
      *
      * @param  string $token
+     * @param  int $code for return code
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    protected function respondWithToken($token)
+    protected function respondWithToken($token, int $code = 200)
     {
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth()->factory()->getTTL() * 60
-        ]);
+        ], $code);
     }
 
+    /**
+     * Creates a new Entry in the User Table
+     * 
+     * @param Request $request
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function register(Request $request)
     {
         $valid = $this->registerValidate($request->all());
         if ($valid !== null) {
             return response()->json($valid, 400);
         }
-        // Log::debug('input', [$check]);
+        User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+        return $this->respondWithToken(auth()->attempt(request(['name', 'password'])), 201);
     }
 
     /**
@@ -109,10 +123,10 @@ class UserController extends Controller
         if ($validator->fails()) {
             return ['message' => $validator->getMessageBag()];
         }
-        if (DB::table('users')->where('name', $request['name'])->first() !== null) {
+        if (User::where('name', $request['name'])->first() !== null) {
             return ['message' => ['name' => ['name already exists']]];
         }
-        if ($check['email'] = DB::table('users')->where('email', $request['email'])->first() !== null) {
+        if (User::where('email', $request['email'])->first() !== null) {
             return ['message' => ['email' => ['email already exists']]];
         }
         return null;
