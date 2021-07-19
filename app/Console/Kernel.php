5356fff7a87a2f632812933ef7f13b9856e21c66
otwald <hinterlanddev@googlemail.com>
2021-07-19 20:31:44 +0200

    added todo marker

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new DailyUpdate)->everyMinute();
+        $schedule->job(new DailyUpdate)->everyMinute(); //TODO change schedule for production
     }
 
     /**
